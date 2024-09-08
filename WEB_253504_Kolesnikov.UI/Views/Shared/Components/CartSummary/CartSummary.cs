0664f2d838fa3196fd17f01d8ded5ad49bc768ab
Pavel Kolesnikov <sirpavelir@mail.ru>
2024-09-08 17:57:15 +0300

    lab_2

---
+++ WEB_253504_Kolesnikov.UI/Views/Shared/Components/CartSummary/CartSummary.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_253504_Kolesnikov.UI.Views.Shared.Components.CartSummary
+{
+    public class CartSummary : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
